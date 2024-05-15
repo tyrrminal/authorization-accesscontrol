30e97fc333ee752f11bf7731f7e64dbb1ee85b24
tyrrminal <mark@tyrrminal.dev>
2024-05-15 15:27:17 -0400

    rename AccessControl to ACL

--- lib/Authorization/AccessControl.pm
+++ lib/Authorization/AccessControl.pm
@@ -2,74 +2,50 @@ package Authorization::AccessControl;
 use v5.26;
 use warnings;
 
-#ABSTRACT: Hybrid RBAC/ABAC access control
+# ABSTRACT: Hybrid RBAC/ABAC access control
 
-use Exporter 'import';
-
-use Authorization::AccessControl::Privilege;
-use Authorization::AccessControl::Predicate;
-use List::Util qw(any);
-use Readonly;
+use Authorization::AccessControl::ACL;
 
 use experimental qw(signatures);
+
+use Exporter 'import';
 
 our @EXPORT_OK = qw(ac);
 
 sub ac() {
-  state $ac = __PACKAGE__->new();
+  state $ac = Authorization::AccessControl::ACL->new();
   $ac;
 }
 
-sub new($class, %params) {
-  Readonly::Hash1 my %data => (
-    _base       => $params{base},
-    _role       => $params{role},
-    _privs      => ($params{base} ? undef : []), # prevent privs from being saved in non-base instances
-  );
-  bless(\%data, $class);
-}
+=head1 AUTHOR
 
-sub clone($self) {
-  my $clone = __PACKAGE__->new();
-  push($clone->{_privs}->@*, $self->{_privs}->@*);
-  return $clone;
-}
+Mark Tyrrell C<< <mark@tyrrminal.dev> >>
 
-sub _base_instance($self) {
-  $self->{_base} // $self
-}
+=head1 LICENSE
 
-sub role($self, $role = undef) {
-  return __PACKAGE__->new(base => $self->_base_instance, role => $role);
-}
+Copyright (c) 2024 Mark Tyrrell
 
-sub grant($self, $resource, $action, $restrictions = undef) {
-  my $p = Authorization::AccessControl::Privilege->new(
-    role         => $self->{_role},
-    resource     => $resource,
-    action       => $action,
-    restrictions => $restrictions,
-  );
-  push($self->_base_instance->{_privs}->@*, $p);
-  return $self
-}
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-sub __contains($arr, $v) {
-  return !1 unless(defined($v));
-  any { $_ eq $v } $arr->@*
-}
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-sub get_grants($self, %filters) {
-  my @grants = $self->_base_instance->{_privs}->@*;
-  @grants = grep { $_->resource eq $filters{resource} } @grants if(exists($filters{resource}));
-  @grants = grep { $_->action   eq $filters{action}   } @grants if(exists($filters{action}));
-  @grants = grep { __contains($filters{roles}, $_->role) || !defined($_->role) } @grants if(exists($filters{roles}));
-  return @grants;
-}
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
-sub roles($self, @roles) {
-  warn("Warning: Calling `roles` on the result of `role` or `grant` calls may not yield expected results\n") if($self->{_base});
-  return Authorization::AccessControl::Predicate->new(access_control => $self->_base_instance, roles => [@roles]);
-}
+=cut
 
 1;
+
+__END__
+
---
+++ lib/Authorization/AccessControl/ACL.pm
@@ -0,0 +1,101 @@
+package Authorization::AccessControl::ACL;
+use v5.26;
+use warnings;
+
+# ABSTRACT: 
+
+use Authorization::AccessControl::Grant;
+use Authorization::AccessControl::Request;
+use List::Util qw(any);
+use Readonly;
+
+use experimental qw(signatures);
+
+sub new($class, %params) {
+  my $base = delete($params{base});
+  my $role = delete($params{role});
+
+  die("Unsupported params: ", join(', ', keys(%params))) if(keys(%params));
+
+  Readonly::Hash1 my %data => (
+    _base   => $base,
+    _role   => $role,
+    _grants => ($base ? undef : []), # prevent privs from being saved in non-base instances
+  );
+  bless(\%data, $class);
+}
+
+sub clone($self) {
+  my $clone = __PACKAGE__->new();
+  push($clone->{_grants}->@*, $self->{_grants}->@*);
+  return $clone;
+}
+
+sub _base_instance($self) {
+  $self->{_base} // $self
+}
+
+sub role($self, $role = undef) {
+  return __PACKAGE__->new(base => $self->_base_instance, role => $role);
+}
+
+sub grant($self, $resource, $action, $restrictions = undef) {
+  my $p = Authorization::AccessControl::Grant->new(
+    role         => $self->{_role},
+    resource     => $resource,
+    action       => $action,
+    restrictions => $restrictions,
+  );
+  push($self->_base_instance->{_grants}->@*, $p);
+  return $self
+}
+
+sub __contains($arr, $v) {
+  return 0 unless(defined($v));
+  any { $_ eq $v } $arr->@*
+}
+
+sub get_grants($self, %filters) {
+  my @grants = $self->_base_instance->{_grants}->@*;
+  @grants = grep { $_->resource eq $filters{resource} } @grants if(exists($filters{resource}));
+  @grants = grep { $_->action   eq $filters{action}   } @grants if(exists($filters{action}));
+  @grants = grep { __contains($filters{roles}, $_->role) || !defined($_->role) } @grants if(exists($filters{roles}));
+  return @grants;
+}
+
+sub request($self) {
+  warn("Warning: Calling `roles` on the result of `role` or `grant` calls may not yield expected results\n") if($self->{_base});
+  return Authorization::AccessControl::Request->new(acl => $self->_base_instance);
+}
+
+=head1 AUTHOR
+
+Mark Tyrrell C<< <mark@tyrrminal.dev> >>
+
+=head1 LICENSE
+
+Copyright (c) 2024 Mark Tyrrell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+=cut
+
+1;
+
+__END__
